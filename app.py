2d2f34c363c95a0e5b589f1a8414fb930a7c3c9b
notshrirang <shrirangmahajan123@gmail.com>
2023-10-20 05:49:06 +0530

    feat: add testing app

---
+++ app.py
@@ -0,0 +1,89 @@
+import streamlit as st
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+from tests import *
+
+TEST_DESCRIPTIONS = {
+    "Check web page title": ["Verify that a web page title matches an expected value.", first_test],
+    "Check presence of Get Started button": ["Verify that a web element is present on a web page.", second_test],
+    "Check login without credentials": ["Verify that no login is possible with empty credentials.", third_test],
+    "Check login with correct credentials": ["Verify that login is possible with correct credentials.", fourth_test],
+    "Check login without username": ["Verify that login is not possible with empty username.", fifth_test],
+    "Check login without password": ["Verify that login is not possible with empty password.", sixth_test],
+    "Check functionality of Get Started button": ["Verify that website goes to login/register page after clicking on 'Get Started' button.", seventh_test],
+    "Check add transaction page": ["Verify that add transaction page is not accessible without login.", eighth_test],
+    "Check transaction view page": ["Verify that transaction view page is not accessible without login.", ninth_test],
+    "Check that logged in user can logout": ["Verify that logged in user can logout.", tenth_test],
+    "Check transaction add page": ["Verify that transaction add page can be accessed by logged in users.", eleventh_test]
+}
+
+@st.cache_resource
+def get_driver():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--log-level=3')
+    chrome_options.add_argument(f'--disable-logging')
+
+    driver = webdriver.Chrome(options=chrome_options)
+    return driver
+
+st.set_page_config(page_title="Budget Project Automated Testing App", page_icon=":moneybag:", layout="wide")
+
+st.title("Budget Project Automated Testing App")
+
+selected_test = st.sidebar.selectbox("Select a Test", list(TEST_DESCRIPTIONS.keys()))
+
+all_tests = st.sidebar.button("Run All Tests")
+
+st.write(f"**Test Description:** {TEST_DESCRIPTIONS[selected_test][0]}")
+
+if st.button("Run Test"):
+    st.write(f"Running test: {TEST_DESCRIPTIONS[selected_test][0]}")
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--log-level=3')
+    chrome_options.add_argument(f'--disable-logging')
+    driver = webdriver.Chrome(options=chrome_options)
+    output = TEST_DESCRIPTIONS[selected_test][1](driver)
+    if output:
+        st.success("Test passed!")
+    else:
+        st.error("Test failed!")
+    driver.quit()
+
+if all_tests:
+    st.write("Running all tests...")
+    options = Options()
+    options.add_argument('--disable-gpu')
+    options.add_argument('--headless')
+    options.add_argument('--disable-blink-features=AutomationControlled')
+    driver = get_driver()
+    outputs = []
+    for test in TEST_DESCRIPTIONS:
+        st.info(f"Running test: {TEST_DESCRIPTIONS[test][0]}")
+        output = TEST_DESCRIPTIONS[test][1](driver)
+        outputs.append(output)
+    driver.quit()
+
+    st.header("Test Plan")
+    st.write("The following table shows the test plan for the DevFinder web application.")
+    test_results = {
+    "Check web page title": "Test Passed" if outputs[0] else "Test Failed",
+    "Check presence of Get Started button": "Test Passed" if outputs[1] else "Test Failed",
+    "Check login without credentials": "Test Passed" if outputs[2] else "Test Failed",
+    "Check login with correct credentials": "Test Passed" if outputs[3] else "Test Failed",
+    "Check login without username": "Test Passed" if outputs[4] else "Test Failed",
+    "Check login without password": "Test Passed" if outputs[5] else "Test Failed",
+    "Check functionality of Get Started button": "Test Passed" if outputs[6] else "Test Failed",
+    "Check developers page": "Test Passed" if outputs[7] else "Test Failed",
+    "Check projects page": "Test Passed" if outputs[8] else "Test Failed",
+    "Check recommendations page": "Test Passed" if outputs[9] else "Test Failed",
+    "Check inbox page": "Test Passed" if outputs[10] else "Test Failed"
+    }
+
+    df = pd.DataFrame()
+    df["Test Name"] = list(test_results.keys())
+    df["Test Description"] = [TEST_DESCRIPTIONS[test][0] for test in TEST_DESCRIPTIONS]
+    df["Test Result"] = list(test_results.values())
+    st.dataframe(df, width=2000)
---
+++ tests.py
@@ -0,0 +1,276 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from colorama import Fore, Back, Style
+from dotenv import load_dotenv
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
+load_dotenv()
+
+BASEURL = "http://localhost:5173"
+
+def first_test(driver):
+    '''
+    Verify that a web page title matches an expected value.
+    '''
+    driver.get(BASEURL)
+    title = driver.title
+    if title == "MMCOE: Budget Management Application":
+        print(Fore.GREEN + "Test passed" + Style.RESET_ALL)
+        return True
+    else:
+        print(Fore.RED + "Test failed" + Style.RESET_ALL)
+        return False
+    
+
+def second_test(driver):
+    '''
+    Verify that a web element is present on a web page.
+    '''
+    driver.get(BASEURL)
+    try:
+        element = driver.find_element(By.XPATH, "/html/body/div/div/div[1]/div[2]/button")
+        if element:
+            print(Fore.GREEN + "Test passed" + Style.RESET_ALL)
+            return True
+        else:
+            print(Fore.RED + "Test failed" + Style.RESET_ALL)
+            return False
+    except Exception as e:
+        print(Fore.RED + f"Test failed with exception: {e.msg}" + Style.RESET_ALL)
+        return False
+    
+
+def third_test(driver):
+    '''
+    Verify that no login is possible with empty credentials.
+    '''
+    driver.get(f"{BASEURL}/login")
+    email_field = driver.find_element(By.XPATH, "/html/body/div/div/div[1]/div[1]/form/div[1]/input")
+    password_field = driver.find_element(By.XPATH, "/html/body/div/div/div[1]/div[1]/form/div[2]/input")
+    email_field.send_keys("")
+    password_field.send_keys("")
+
+    login_button = driver.find_element(By.XPATH, "/html/body/div/div/div[1]/div[1]/form/button")
+    login_button.click()
+    expected_url = f"{BASEURL}/transactions"
+    try:
+        WebDriverWait(driver, 5).until(EC.url_to_be(expected_url))
+        if driver.current_url == expected_url:
+            print(Fore.RED + "Test failed" + Style.RESET_ALL)
+            return False
+        else:
+            print(Fore.GREEN + "Test passed" + Style.RESET_ALL)
+            return True
+    except Exception as e:
+        print(Fore.GREEN + f"Test passed" + Style.RESET_ALL)
+        return True
+    
+
+def fourth_test(driver):
+    '''
+    Verify that login is possible with correct credentials.
+    '''
+    driver.get(f"{BASEURL}/login")
+    email_field = driver.find_element(By.XPATH, "/html/body/div/div/div[1]/div[1]/form/div[1]/input")
+    password_field = driver.find_element(By.XPATH, "/html/body/div/div/div[1]/div[1]/form/div[2]/input")
+    email_field.send_keys(os.getenv("BUDGET_PROJECT_USERNAME"))
+    password_field.send_keys(os.getenv("BUDGET_PROJECT_PASSWORD"))
+
+    login_button = driver.find_element(By.XPATH, "/html/body/div/div/div[1]/div[1]/form/button")
+    login_button.click()
+    expected_url = f"{BASEURL}/transactions"
+    try:
+        WebDriverWait(driver, 20).until(EC.url_to_be(expected_url))
+        if driver.current_url == expected_url:
+            print(Fore.GREEN + "Test passed" + Style.RESET_ALL)
+            sign_out_button = driver.find_element(By.XPATH, "/html/body/div/div/div[1]/div[1]/div/div[2]/div")
+            sign_out_button.click()
+            yes_button = driver.find_element(By.XPATH, "/html/body/div[2]/div/div[6]/button[1]")
+            yes_button.click()
+            return True
+        else:
+            print(Fore.RED + "Test failed" + Style.RESET_ALL)
+            return False
+    except Exception as e:
+        print(Fore.RED + f"Test failed because of unexpected result" + Style.RESET_ALL)
+        return False
+    
+
+def fifth_test(driver):
+    '''
+    Verify that login is not possible with empty username.
+    '''
+    driver.get(f"{BASEURL}/login")
+    email_field = driver.find_element(By.XPATH, "/html/body/div/div/div[1]/div[1]/form/div[1]/input")
+    password_field = driver.find_element(By.XPATH, "/html/body/div/div/div[1]/div[1]/form/div[2]/input")
+    email_field.send_keys("")
+    password_field.send_keys(os.getenv("BUDGET_PROJECT_PASSWORD"))
+
+    login_button = driver.find_element(By.XPATH, "/html/body/div/div/div[1]/div[1]/form/button")
+    login_button.click()
+    expected_url = f"{BASEURL}/account"
+    try:
+        WebDriverWait(driver, 5).until(EC.url_to_be(expected_url))
+        if driver.current_url == expected_url:
+            print(Fore.RED + "Test failed" + Style.RESET_ALL)
+            return False
+        else:
+            print(Fore.GREEN + "Test passed" + Style.RESET_ALL)
+            return True
+    except Exception as e:
+        print(Fore.GREEN + f"Test passed" + Style.RESET_ALL)
+        return True
+
+
+def sixth_test(driver):
+    '''
+    Verify that login is not possible with empty password.
+    '''
+    driver.get(f"{BASEURL}/login")
+    email_field = driver.find_element(By.XPATH, "/html/body/div/div/div[1]/div[1]/form/div[1]/input")
+    password_field = driver.find_element(By.XPATH, "/html/body/div/div/div[1]/div[1]/form/div[2]/input")
+    email_field.send_keys(os.getenv("BUDGET_PROJECT_USERNAME"))
+    password_field.send_keys("")
+
+    login_button = driver.find_element(By.XPATH, "/html/body/div/div/div[1]/div[1]/form/button")
+    login_button.click()
+    expected_url = f"{BASEURL}/account"
+    try:
+        WebDriverWait(driver, 5).until(EC.url_to_be(expected_url))
+        if driver.current_url == expected_url:
+            print(Fore.RED + "Test failed" + Style.RESET_ALL)
+            return False
+        else:
+            print(Fore.GREEN + "Test passed" + Style.RESET_ALL)
+            return True
+    except Exception as e:
+        print(Fore.GREEN + f"Test passed" + Style.RESET_ALL)
+        return True
+
+
+def seventh_test(driver):
+    '''
+    Verify that website goes to login/register page after clicking on "Get Started" button.
+    '''
+    driver.get(BASEURL)
+    get_started_button = driver.find_element(By.XPATH, "/html/body/div/div/div[1]/div[2]/button")
+    get_started_button.click()
+    expected_url = f"{BASEURL}/login"
+    try:
+        WebDriverWait(driver, 5).until(EC.url_to_be(expected_url))
+        if driver.current_url == expected_url:
+            print(Fore.GREEN + "Test passed" + Style.RESET_ALL)
+            return True
+        else:
+            print(Fore.RED + "Test failed" + Style.RESET_ALL)   
+            return False
+    except Exception as e:
+        print(Fore.RED + f"Test failed because of unexpected result" + Style.RESET_ALL)
+        return False
+
+
+def eighth_test(driver):
+    '''
+    Verify that add transaction page is not accessible.
+    '''
+    try:
+        driver.get(f"{BASEURL}/transactions/add")
+        redirected_url = WebDriverWait(driver, 5).until(
+            EC.url_matches(BASEURL+"/login")
+        )
+
+        if redirected_url:
+            print(Fore.GREEN + "Test passed: Redirects to login page." + Style.RESET_ALL)
+            return True
+        else:
+            print(Fore.RED + f"Test failed - Redirected to: {BASEURL}/transactions/add" + Style.RESET_ALL)
+            return False
+    except Exception as e:
+        print(Fore.RED + f"Test failed because of unexpected error: {e}" + Style.RESET_ALL)
+        return False
+
+
+def ninth_test(driver):
+    '''
+    Verify that transactions page is not accessible without login.
+    '''
+    try:
+        driver.get(f"{BASEURL}/transactions/")
+        redirected_url = WebDriverWait(driver, 5).until(
+            EC.url_matches(BASEURL+"/login")
+        )
+
+        if redirected_url:
+            print(Fore.GREEN + "Test passed" + Style.RESET_ALL)
+            return True
+        else:
+            print(Fore.RED + f"Test failed - Redirected to: {BASEURL}/transactions/add" + Style.RESET_ALL)
+            return False
+    except Exception as e:
+        print(Fore.RED + f"Test failed because of unexpected error: {e}" + Style.RESET_ALL)
+        return False
+
+def tenth_test(driver):
+    '''
+    Verify that logged in user can logout.
+    '''
+    if fourth_test(driver):
+        sign_out_button = driver.find_element(By.XPATH, "/html/body/div/div/div[1]/div[1]/div/div[2]/div")
+        sign_out_button.click()
+        yes_button = driver.find_element(By.XPATH, "/html/body/div[2]/div/div[6]/button[1]")
+        yes_button.click()
+        expected_url = f"{BASEURL}/login"
+        try:
+            WebDriverWait(driver, 5).until(EC.url_to_be(expected_url))
+            if driver.current_url == expected_url:
+                print(Fore.GREEN + "Test passed" + Style.RESET_ALL)
+                return True
+            else:
+                print(Fore.RED + "Test failed" + Style.RESET_ALL)
+                return False
+        except Exception as e:
+            print(Fore.RED + f"Test failed because of unexpected result" + Style.RESET_ALL)
+            return False
+    else:
+        print(Fore.RED + f"Test failed because of wrong login credentials" + Style.RESET_ALL)
+        return False
+
+def eleventh_test(driver):
+    '''
+    Verify that only logged in user can add transaction.
+    '''
+    print("expected_url")
+    if fourth_test(driver):
+        add_button = driver.find_element(By.XPATH, "/html/body/div/div/div[1]/div[2]/div[2]/div/div[1]/div[1]/div[2]")
+        add_button.click()
+        expected_url = f"{BASEURL}/transactions/add"
+        try:
+            WebDriverWait(driver, 20).until(EC.url_to_be(expected_url))
+            if driver.current_url == expected_url:
+                print(Fore.GREEN + "Test passed" + Style.RESET_ALL)
+                return True
+            else:
+                print(Fore.RED + "Test failed" + Style.RESET_ALL)
+                return False
+        except Exception as e:
+            print(Fore.RED + f"Test failed because of unexpected result" + Style.RESET_ALL)
+            return False
+    else:
+        print(Fore.RED + f"Test failed because of wrong login credentials" + Style.RESET_ALL)
+        return False
+
+if __name__ == "__main__":
+    driver = webdriver.Chrome()
+    first_test(driver)
+    second_test(driver)
+    third_test(driver)
+    fourth_test(driver)
+    fifth_test(driver)
+    sixth_test(driver)
+    seventh_test(driver)
+    eighth_test(driver)
+    ninth_test(driver)
+    tenth_test(driver)
+    eleventh_test(driver)
+    driver.quit()
